1139d45430ff87e106bcac1fb52b5a0e97708866
Вартикян Михаил <m.vartikyan@bk.ru>
2024-01-22 14:53:40 +0300

    Добавление клавиатуры для выбора периода опроса

--- src/keyboards.py
+++ src/keyboards.py
@@ -1,17 +1,21 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.types import InlineKeyboardButton
 
-main_keyboard = InlineKeyboardBuilder()
 
-main_keyboard.add(InlineKeyboardButton(text="✅Начать отслеживание", callback_data="run"))
-main_keyboard.add(InlineKeyboardButton(text="🚫Остановить отслеживание", callback_data="stop"))
-main_keyboard.add(InlineKeyboardButton(text="➕Добавить сайт", callback_data="add_site"))
-main_keyboard.add(InlineKeyboardButton(text="➖Удалить сайт", callback_data="delete_site"))
-main_keyboard.add(InlineKeyboardButton(text="🗂️Список сайтов", callback_data="get_sites"))
-main_keyboard.add(InlineKeyboardButton(text="🗑️Очистить список", callback_data="clear_sites"))
-main_keyboard.add(InlineKeyboardButton(text="ℹ️Статус", callback_data="status"))
+def main_keyboard() -> InlineKeyboardBuilder:
+    keyboard = InlineKeyboardBuilder()
 
-main_keyboard.adjust(1, 1, 2, 2)
+    keyboard.add(InlineKeyboardButton(text="✅Начать отслеживание", callback_data="run"))
+    keyboard.add(InlineKeyboardButton(text="🚫Остановить отслеживание", callback_data="stop"))
+    keyboard.add(InlineKeyboardButton(text="🕒️Периодичность опроса", callback_data="period"))
+    keyboard.add(InlineKeyboardButton(text="➕Добавить сайт", callback_data="add_site"))
+    keyboard.add(InlineKeyboardButton(text="➖Удалить сайт", callback_data="delete_site"))
+    keyboard.add(InlineKeyboardButton(text="🗂️Список сайтов", callback_data="get_sites"))
+    keyboard.add(InlineKeyboardButton(text="🗑️Очистить список", callback_data="clear_sites"))
+    keyboard.add(InlineKeyboardButton(text="ℹ️Статус", callback_data="status"))
+
+    keyboard.adjust(1, 1, 1, 2, 2)
+    return keyboard
 
 
 def url_keyboard_factory(sites: list[str]) -> InlineKeyboardBuilder:
@@ -22,3 +26,17 @@ def url_keyboard_factory(sites: list[str]) -> InlineKeyboardBuilder:
     url_keyboard.add(InlineKeyboardButton(text="❌Отмена", callback_data="menu"))
     url_keyboard.adjust(1)
     return url_keyboard
+
+
+def cron_keyboard() -> InlineKeyboardBuilder:
+    keyboard = InlineKeyboardBuilder()
+    minutes = (1, 5, 10, 15, 30, 60)
+
+    for time in minutes:
+        keyboard.add(
+            InlineKeyboardButton(text=f"{time} минут{'а' if time == 1 else ''}", callback_data=f"period_{time}_minute")
+        )
+
+    keyboard.add(InlineKeyboardButton(text="❌Отмена", callback_data="menu"))
+    keyboard.adjust(2)
+    return keyboard
